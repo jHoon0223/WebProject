6f64ae3a7a9593671427bc747e560863f660048a
jHoon0223 <aleejaehun@naver.com>
2021-02-02 12:38:28 +0900

    210202 : deleted files

--- menuBar/active.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.toggle');
-const menu = document.querySelector('.member');
-
-toggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
